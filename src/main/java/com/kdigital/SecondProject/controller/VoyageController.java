d982de7c19a279d38417a5f7db692717c32530ab
oddlyk <kimmyok1941@gmail>
2024-10-18 11:34:12 +0900

    항해 수정중

--- src/main/java/com/kdigital/SecondProject/controller/VoyageController.java
+++ src/main/java/com/kdigital/SecondProject/controller/VoyageController.java
@@ -1,7 +1,12 @@
 package com.kdigital.SecondProject.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kdigital.SecondProject.dto.LoginUserDetails;
 import com.kdigital.SecondProject.service.AISService;
 import com.kdigital.SecondProject.service.VoyageService;
 
@@ -22,7 +27,17 @@ public class VoyageController {
 
 	/**
 	 * 콜사인을 포함하여 항해 정보 세부페이지로
-	 * @
+	 * @callSign
+	 * @return  shipInfo.html
 	 * */
+	@GetMapping({"voyage/voyagedetail"})
+	public String voyageDetail(
+			@AuthenticationPrincipal  LoginUserDetails loginUser, //인증받은 사용자가 있다면 그 정보를 담아옴
+			@RequestParam(name="callSign", defaultValue="-1") String callSign, //검색버튼 클릭 시
+			Model model
+			) {
+		return "/pages/shipInfo";
+	}
+	
 	
 }
